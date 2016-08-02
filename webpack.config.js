d6e861e140fee94e7e706d7726abe5de6f95387d
Zachary Porter <zporter9@gmail.com>
2016-08-02 14:59:35 -0400

    Configure webpack to handle CSS separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
+// We need to bring in the ExtractTextPlugin. This will allow us to pull the
+// CSS out of our bundle and output it to its own file.
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: "./web/static/js/app.js",
+  // We'll add an entry point for CSS, which means our `entry` field must now
+  // be an array
+  entry: ["./web/static/css/app.css", "./web/static/js/app.js"],
+  // since we're no longer just outputting js, we'll tweak our output path and
+  // output filename for js.
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
+    path: "./priv/static",
+    filename: "js/app.js"
   },
   module: {
     loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel",
-      query: {
-        presets: ["es2015"]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["es2015"]
+        }
+      },
+      // We'll add a css loader
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", "css")
       }
-    }]
+    ]
   },
+  // And we'll add the plugin configuration
+  plugins: [
+    new ExtractTextPlugin("css/app.css")
+  ],
   resolve: {
     modulesDirectories: [
       "node_modules",
